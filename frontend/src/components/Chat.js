b532b3a5a6fe4f1cb5a76ac231d7b67021e7b7c4
Busayo Dada <toyinoluwabusayo@gmail.com>
2023-03-17 15:27:17 +0100

    update to frontend

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import config from '../chatbot/config';
 import MessageParser from '../chatbot/MessageParser'
 import ActionProvider from '../chatbot/ActionProvider';
